459139a15b20290202a02abe70a603e3ffc413ca
elvisilunga <elvisilunga@users.noreply.github.com>
2016-01-10 19:05:12 +0200

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,8 @@
+## Reading in the data ##
+hpw <- read.table("household_power_consumption.txt", header=TRUE, sep=";",col.names=c("Date","Time","Global_active_power","Global_reactive_power","Volage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),na.strings="NA",colClasses="character")
+## Subsetting the relevant data (01-20-2007 to 02-02-2007) ##
+hpw2 <- hpw[66637:69516,]
+
 ## Starting by creating a png object ##
 png(filename = "Plot4.png", width = 480, height = 480, units = "px")
 ## Then comes the command for multiple plots (two plots in a row and two in column)##
